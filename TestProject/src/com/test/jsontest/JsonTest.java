fe5ae9ae5b0393b5fb557a6372d64094211b2cd0
Sanath <gsanathkumar@hotmail.com>
2017-12-10 10:02:51 +0530

    made changes

--- TestProject/src/com/test/jsontest/JsonTest.java
+++ TestProject/src/com/test/jsontest/JsonTest.java
@@ -1,30 +1,8 @@
 package com.test.jsontest;
 
-import com.google.gson.Gson;
-
 public class JsonTest {
 
 	public static void main(String[] args) {
-		System.out.println("Test pull from GIT");
-		Gson gson = new Gson();
-		String json = "{"+
-			"\"name\": \"John\","+
-			"\"age\": 27,"+
-			"\"interests\": [\"running\", \"guitar\", \"movies\"],"+
-			"\"favorites\": [{"+
-					"\"color\": \"blue\","+
-					"\"book\": \"First: Bach: An Eternal Golden Braid\""+
-				"},"+
-				"{"+
-					"\"color\": \"Yellow\","+
-					"\"book\": \"Second: Bach: An Eternal Golden Braid\""+
-				"},"+
-				"{"+
-					"\"color\": \"Green\","+
-					"\"book\": \"Third: An Eternal Golden Braid\""+
-				"}]"+
-		"}";
-		Details d = gson.fromJson(json, Details.class);
-		System.out.println(d);
+		System.out.println("First jenkins build program");
 	}
 }
